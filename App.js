aa9192783c8760bfb0cce38559fde46f9d0e5096
Humayun Sheikh <huayunsheikh221@gmail.com>
2021-04-19 22:55:57 +0500

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View ,Dimensions } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+
+const { width } = Dimensions.get('window')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <StatusBar translucent backgroundColor='rgba(0,0,0,0)' />
+
+      <View style={styles.sliderContainer}>
+        <Swiper
+          autoplay
+          horizontal={false}
+          height={200}
+          activeDotColor="#FF6347">
+          <View style={styles.slide}>
+            <Image
+              source={require('./assets/coob.png')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('./assets/coob2.jpg')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('./assets/coob2.jpg')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+        </Swiper>
+      </View>
+
+      <Text>Hello</Text>
+
+
     </View>
   );
 }
@@ -15,7 +53,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ 
   },
+
+  sliderContainer: {
+    height: 200,
+    width: '90%',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+
+  wrapper: {},
+
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: 8,
+  },
+  sliderImage: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+
 });
